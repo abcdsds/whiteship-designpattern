6c85511bcb3bb513c3e68e961eae6f432a709746
abcdsds <choiyurim2@naver.com>
2021-11-27 18:34:21 +0900

    feat: add mediator pattern

---
+++ src/main/java/mediator/Restaurant.java
@@ -0,0 +1,14 @@
+package mediator;
+
+public class Restaurant {
+
+    private CleaningService cleaningService = new CleaningService();
+
+    public void dinner(Guest guest) {
+        System.out.println("dinner " + guest);
+    }
+
+    public void clean() {
+        cleaningService.clean(this);
+    }
+}
